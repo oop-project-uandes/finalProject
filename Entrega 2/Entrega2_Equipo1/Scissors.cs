6e5d027ce1f4753e580c6e7aebe415872505f653
Gianfranco <glacasella@miuandes.cl>
2019-09-02 16:53:32 -0400

    Agregando detalles

--- Entrega 2/Entrega2_Equipo1/Scissors.cs
+++ Entrega 2/Entrega2_Equipo1/Scissors.cs
@@ -7,11 +7,17 @@ namespace Entrega2_Equipo1
 {
     public class Scissors
     {
+        
         public Scissors() { }
 
         public List<System.Drawing.Bitmap> Cut(Dictionary<System.Drawing.Bitmap, double[4]> images)
         {
             throw new NotImplementedException("Not implemented yet");
         }
+        
     }
 }
+
+
+
+
