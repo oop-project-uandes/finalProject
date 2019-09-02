c8dba99ddee4a668831f371219cb69bfab7cb56c
Gianfranco <glacasella@miuandes.cl>
2019-09-02 10:33:36 -0400

    Agregados los archivos WatsonFilter.cs y Scissors.cs

---
+++ Entrega 2/Entrega2_Equipo1/Scissors.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace Entrega2_Equipo1
+{
+    public class Scissors
+    {
+        public Scissors() { }
+
+        public List<System.Drawing.Bitmap> Cut(Dictionary<System.Drawing.Bitmap, double[4]> images)
+        {
+            throw new NotImplementedException("Not implemented yet");
+        }
+    }
+}
--- Entrega 2/Entrega2_Equipo1/WatsonFilter.cs
+++ Entrega 2/Entrega2_Equipo1/WatsonFilter.cs
@@ -6,5 +6,20 @@ namespace Entrega2_Equipo1
 {
     class WatsonFilter
     {
+        private Scissors scissors;
+        private Dictionary<string, string> LastRequest;
+
+        public WatsonFilter() { }
+
+        public Dictionary<string, string> applyFilter(System.Drawing.Bitmap image)
+        {
+            throw new NotImplementedException("Not implemented yet");
+        }
+
+        public List<System.Drawing.Bitmap> CutFaces(System.Drawing.Bitmap image)
+        {
+            throw new NotImplementedException("Not implemented yet");
+        }
+
     }
 }
