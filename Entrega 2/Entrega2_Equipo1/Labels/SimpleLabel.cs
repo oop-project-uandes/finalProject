4f47d8fca3bc05a97c900f03bfcb93e80a2d6f86
Gianfranco <glacasella@miuandes.cl>
2019-09-04 22:39:39 -0400

    Trabajando en Labels

---
+++ Entrega 2/Entrega2_Equipo1/Labels/SimpleLabel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrega2_Equipo1
+{
+    public class SimpleLabel : Label
+    {
+        private const int DEFAULT_MAX_LENGTH_SENTENCE = 20;
+        private string sentence;
+        private int serialNumber;
+
+        public SimpleLabel() : base()
+        { }
+
+        public SimpleLabel(string sentence) : base()
+        {
+            this.sentence = sentence;
+        }
+
+        public SimpleLabel(string sentence, int serialNumber) : base()
+        {
+            this.sentence = sentence;
+            this.serialNumber = serialNumber;
+        }
+
+        public string Sentence
+        {
+            get => this.sentence;
+            set
+            {
+                if (value.Length > DEFAULT_MAX_LENGTH_SENTENCE)
+                {
+                    throw new Exception("Sentence too long. Didn't assign the Label");
+                }
+                else
+                {
+                    this.sentence = value;
+                }
+            }
+        }
+        
+        public int SerialNumber
+        {
+            get => this.serialNumber;
+            set => this.serialNumber = value;
+        }
+
+    }
+}
---
+++ Entrega 2/Entrega2_Equipo1/Labels/SpecialLabel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrega2_Equipo1.Labels
+{
+    public class SpecialLabel : Label
+    {
+        private double[] geographicLocation;
+        private readonly double[] DEFAULT_GEOGRAPHIC_LOCATION = null;
+        private string address;
+        private const string DEFAULT_ADDRESS = null;
+        private string photographer;
+        private const string DEFAULT_PHOTOGRAPHER = null;
+        private string photoMotive;
+        private const string DEFAULT_PHOTOMOTIVE = null;
+        private bool selfie;
+        private const bool DEFAULT_SELFIE = false;
+        private int serialNumber;
+
+        // Falta trabajar en los getters y setters. En algunos como la ubicacion geografica se debe revisar que
+        // sea valido el valor que se intenta ingresar.
+    }
+}
