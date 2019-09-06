fef0b4695fa12593eded8243e1911ff12b68ca95
Gianfranco <glacasella@miuandes.cl>
2019-09-06 13:45:42 -0400

    Trabajando en filtros

--- Entrega 2/Entrega2_Equipo1/Program.cs
+++ Entrega 2/Entrega2_Equipo1/Program.cs
@@ -12,9 +12,10 @@ namespace Entrega2_Equipo1
         static void Main(string[] args)
         {
             Bitmap imagen1 = new Bitmap("C:\\Users\\Gianfranco Lacasella\\Desktop\\Prueba c# watson\\imagen1.jpg");
-            MirrorFilter filter = new MirrorFilter();
-            Bitmap SP = filter.ApplyFilter(imagen1);
-            SP.Save("C:\\Users\\Gianfranco Lacasella\\Desktop\\Prueba c# watson\\mirror.jpg");
+            Brush brush = new Brush();
+            Color color = Color.FromArgb(255, 0, 0);
+            Bitmap SP = brush.paintRectangle(imagen1, color, new int[] { 400,600,20,20});
+            SP.Save("C:\\Users\\Gianfranco Lacasella\\Desktop\\Prueba c# watson\\painted.jpg");
         }
     }
 }
---
+++ Entrega 2/Entrega2_Equipo1/Tools/Brush.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrega2_Equipo1
+{
+    public class Brush : Tool
+    {
+        public Brush() { }
+
+        // Method to paint a rectangle over an image
+        // Coordinates must be given as (X,Y,TOP,LEFT)
+        public Bitmap paintRectangle(Bitmap image, Color color, int[] coordinates)
+        {
+            if ((coordinates[0] + coordinates[3] > image.Width) || (coordinates[1] + coordinates[2] > image.Height)) throw new Exception("Not valid coordinates");
+            Bitmap copy = (Bitmap)image.Clone();
+            for (int i = coordinates[2]; i < coordinates[2] +coordinates[1]; i++)
+            {
+                for (int x = coordinates[3]; x < coordinates[3] +coordinates[0]; x++)
+                {
+                    copy.SetPixel(x, i, color);
+                }
+            }
+            return copy;
+        }
+    }
+}
