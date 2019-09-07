e49b339116a2c8ebb155ab479b0540006b090870
Gianfranco <glacasella@miuandes.cl>
2019-09-07 16:14:26 -0400

    Queseyo

--- Entrega 2/Entrega2_Equipo1/Image.cs
+++ Entrega 2/Entrega2_Equipo1/Image.cs
@@ -20,20 +20,31 @@ namespace Entrega2_Equipo1
         private double saturation;
         private bool darkClear;
         private Dictionary<string, string> exif;
-        
 
+        public List<Label> Labels { get => this.labels; set => this.labels = value; }
+        public string Name { get => this.name; set => this.name = value; }
+        public Bitmap Image { get => this.image; set => this.image = value; }
+        public int Calification { get => this.calification; set => this.calification = value; }
+        public int[] Resolution { get => this.resolution; set => this.resolution = value; }
+        public int[] AspectRatio { get => this.aspectRatio; set => this.aspectRatio = value; }
+        public double Saturation { get => this.saturation; set => this.saturation = value; }
+        public bool DarkClear { get => this.darkClear; set => this.darkClear = value; }
+        public Dictionary<string, string> Exif { get => this.exif; set => this.exif = value; }
 
         public Image(string name, List<Label> labels, int calification)
         {
-            this.name = name;
-            this.labels = labels;
-            this.calification = calification;
-            this.image = LoadImage(name);
-            this.resolution = LoadResolution();
-            this.aspectRatio = LoadAspectRatio();
-            this.saturation = LoadSaturation();
-            this.darkClear = LoadDarkClear();
+            this.Name = name;
+            this.Labels = labels;
+            this.Calification = calification;
+            this.Image = LoadImage(name);
+            this.Resolution = LoadResolution();
+            this.AspectRatio = LoadAspectRatio();
+            this.Saturation = LoadSaturation();
+            this.DarkClear = LoadDarkClear();
         }
+
+        public Image()
+        { }
 
         private Bitmap LoadImage(string name)
         {
@@ -44,8 +55,8 @@ namespace Entrega2_Equipo1
 
         private int[] LoadResolution()
         {
-            int v1 = this.image.Width;
-            int v2 = this.image.Height;
+            int v1 = this.Image.Width;
+            int v2 = this.Image.Height;
             int[] returningArray = new int[] { v1, v2 };
             return returningArray;
         }
@@ -70,7 +81,7 @@ namespace Entrega2_Equipo1
 
         private int[] LoadAspectRatio()
         {
-            int[] returningAspect = new int[] { this.image.Width, this.image.Height };
+            int[] returningAspect = new int[] { this.Image.Width, this.Image.Height };
             Simplify(returningAspect);
             return returningAspect;
         }
@@ -78,14 +89,14 @@ namespace Entrega2_Equipo1
 
         private double LoadSaturation()
         {
-            double[] aux = new double[image.Width*image.Width];
+            double[] aux = new double[Image.Width*Image.Width];
             Color color;
             int counter = 0;
-            for (int i = 0; i < this.image.Height; i++)
+            for (int i = 0; i < this.Image.Height; i++)
             {
-                for (int x = 0; x < this.image.Width; x++)
+                for (int x = 0; x < this.Image.Width; x++)
                 {
-                    color = this.image.GetPixel(x, i);
+                    color = this.Image.GetPixel(x, i);
                     double v1 = Math.Max(Math.Max(color.R, color.G), color.B);
                     double v2 = Math.Min(Math.Min(color.R, color.G), color.B);
                     aux[counter] = Math.Acos((v1 + v2) / v1);
@@ -99,17 +110,17 @@ namespace Entrega2_Equipo1
         {
             Color color;
             double brightness, totalBrightness = 0;
-            for (int i = 0; i < this.image.Height; i++)
+            for (int i = 0; i < this.Image.Height; i++)
             {
-                for (int x = 0; x < this.image.Width; x++)
+                for (int x = 0; x < this.Image.Width; x++)
                 {
-                    color = this.image.GetPixel(x, i);
+                    color = this.Image.GetPixel(x, i);
                     brightness = (color.R * 299 + color.G * 587 + color.B * 114) / 1000;
                     totalBrightness += brightness;
                 }
             }
 
-            double result = totalBrightness / (this.image.Width * this.image.Height);
+            double result = totalBrightness / (this.Image.Width * this.Image.Height);
             if (result < 0.5)
             {
                 return false;
@@ -120,10 +131,23 @@ namespace Entrega2_Equipo1
             }
         }
 
-        private Dictionary<string, string> LoadExif()
+        public void LoadExif(Bitmap image)
         {
-            
-            
+            PropertyItem[] items = image.PropertyItems;
+            string id;
+            string type;
+            string len;
+            string value;
+            foreach (PropertyItem item in items)
+            {
+                ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+                id = item.Id.ToString();
+                type = item.Id.ToString();
+                len = item.Len.ToString();
+                value = encoding.GetString(item.Value);
+                Console.WriteLine($"0x{id}, {type}, {len}, {value}");
+
+            }
         }
 
     }
