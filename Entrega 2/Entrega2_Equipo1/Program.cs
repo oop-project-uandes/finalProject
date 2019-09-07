193aa9a97d67ab68efd93a054b8b485475ae503e
Gianfranco <glacasella@miuandes.cl>
2019-09-07 10:25:03 -0400

    Agregadas imagenes stock

--- Entrega 2/Entrega2_Equipo1/Program.cs
+++ Entrega 2/Entrega2_Equipo1/Program.cs
@@ -12,7 +12,6 @@ namespace Entrega2_Equipo1
         static void Main(string[] args)
         {
             Bitmap imagen1 = new Bitmap("C:\\Users\\Gianfranco Lacasella\\Desktop\\Prueba c# watson\\imagen1.jpg");
-            Brush brush = new Brush();
             Color color = Color.FromArgb(255, 0, 0);
             Bitmap SP = brush.paintRectangle(imagen1, color, new int[] { 400,600,20,20});
             SP.Save("C:\\Users\\Gianfranco Lacasella\\Desktop\\Prueba c# watson\\painted.jpg");
