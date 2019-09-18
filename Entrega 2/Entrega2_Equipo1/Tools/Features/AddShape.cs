0a89554b9d7a837b1f22579d008635f792b5d846
MatiasRiveraC <mvrivera@miuandes.cl>
2019-09-18 14:10:02 -0300

    ADD Image/Text/Shape/Collage

--- Entrega 2/Entrega2_Equipo1/Tools/Features/AddShape.cs
+++ Entrega 2/Entrega2_Equipo1/Tools/Features/AddShape.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,33 @@ namespace Entrega2_Equipo1
     // TODO: IMPLEMENTAR
     public class AddShape : Tool
     {
+        public Bitmap InsertShape(Bitmap bitmap,string shapeType, int xAxis,
+    int yAxis, int width=150, int height=100, string colorName= null)
+        {
+            Bitmap temp = (Bitmap)bitmap.Clone();
+            Graphics gr = Graphics.FromImage(temp);
+            if (string.IsNullOrEmpty(colorName))
+                colorName = "Black";
+            Pen pen = new Pen(Color.FromName(colorName));
+            switch (shapeType.ToLower())
+            {
+                case "filledellipse":
+                    gr.FillEllipse(pen.Brush, xAxis,
+              yAxis, width, height);
+                    break;
+                case "filledrectangle":
+                    gr.FillRectangle(pen.Brush, xAxis,
+              yAxis, width, height);
+                    break;
+                case "ellipse":
+                    gr.DrawEllipse(pen, xAxis, yAxis, width, height);
+                    break;
+                case "rectangle":
+                    gr.DrawRectangle(pen, xAxis, yAxis, width, height);
+                    break;
 
+            }
+            return temp;
+        }
     }
 }
