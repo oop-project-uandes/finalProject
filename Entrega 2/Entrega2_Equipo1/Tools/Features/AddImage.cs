e063f34447d828b7af167ac7e69b8445b74858e5
MatiasRiveraC <mvrivera@miuandes.cl>
2019-09-26 18:39:43 -0300

    MOSAIC WIP

--- Entrega 2/Entrega2_Equipo1/Tools/Features/AddImage.cs
+++ Entrega 2/Entrega2_Equipo1/Tools/Features/AddImage.cs
@@ -52,99 +52,101 @@ namespace Entrega2_Equipo1
         }
 
 
-		public Bitmap ReadColors(Image image, int width =10, int height = 10)
+		public Bitmap ReadColors(Image image, List<Image> images, int width =8, int height = 3)
 		{
 			//Converting loaded image into bitmap
 			Resizer resizer = new Resizer();
 			Bitmap imageBit = image.BitmapImage;
 			Bitmap bmp = (Bitmap)imageBit.Clone();
-			bmp = resizer.ResizeImage(bmp, 1000, 1000);
+            Scissors scissors = new Scissors();
+			bmp = resizer.ResizeImage(bmp, width*100, height*100);
+
+            List<Bitmap> list = new List<Bitmap>();
+            for (int i = 0; i < (bmp.Width/width); i++)
+            {
+                for (int y = 0; y < (bmp.Height/height); y++)
+                {
+                    Graphics gr = Graphics.FromImage(bmp);
+                    Rectangle r = new Rectangle(i * (bmp.Width / (bmp.Width/width)),
+                                                y * (bmp.Height / (bmp.Height/height)),
+                                                bmp.Width / (bmp.Width/width),
+                                                bmp.Height / (bmp.Height/height));
+                    double[] coord = { r.X, r.Y, r.Width, r.Height };
+                    list.Add(scissors.Crop(bmp, coord)); 
+
+                }
+            }
+            List<int[]> rgbAVG = avgRGB(list);
+            
+            Console.WriteLine(rgbAVG.Count);
+
+            List<Bitmap> imagesList = new List<Bitmap>();
+            if (imagesList.Count !=0)
+            {
+                foreach (Image imageInsert in images)
+                {
+                    imagesList.Add(imageInsert.BitmapImage);
+                }
+            }
 
 
-			List<int[]> TotalRGB= new List<int[]>();
-			for (int i = 0; i < bmp.Height; i++)
-			{
 
-				for (int j = 0; j < bmp.Width; j++)
-				{
-					Color now_color = bmp.GetPixel(j, i);
-
-					int[] RGB = { (int)now_color.R, (int)now_color.G, (int)now_color.B };
-					TotalRGB.Add(RGB);
-
-				}
-			}
-
-			int wCont = 0;
-
-			List<List<int[]>> RGBbyBlocks = new List<List<int[]>>();
-			List<int[]> subBlock = new List<int[]>();
-			for (int i=0; i<TotalRGB.Count; i++)
-			{
-				if ((wCont % width)==0 && wCont != 0)
-				{
-					List<int[]> cloneList = subBlock.ToList();
-					RGBbyBlocks.Add(cloneList);
-					subBlock.Clear();
-					wCont = 0;
-					
-				}
-				
-					subBlock.Add(TotalRGB[i]);
-					wCont++;
-				
-			}
-
-			int hCont = 0;
-			List<List<List<int[]>>> Cells = new List<List<List<int[]>>>();
-			List<List<int[]>> temp = new List<List<int[]>>();
-			foreach (List<int[]> subList in RGBbyBlocks)
-			{
-				if ((hCont % height) == 0 && hCont != 0)
-				{
-					List<List<int[]>> cloneList = temp.ToList();
-					Cells.Add(cloneList);
-					temp.Clear();
-					hCont = 0;
-				}
-				temp.Add(subList);
-				hCont++;
-
-			}
-
-			int R = 0;
-			int G = 0;
-			int B = 0;
-
-			List<int[]> avgRGB = new List<int[]>();
-			foreach (List<List<int[]>> Altura in Cells)
-			{
-				foreach(List<int[]> Ancho in Altura)
-				{
-					foreach (int[] rgb in Ancho)
-					{ 
-						R += rgb[0];
-						G += rgb[1];
-						B += rgb[2];
-
-					}
-				}
-				R = R / (width * height);
-				G = G / (width * height);
-				B = B / (width * height);
-				int[] rgbs = { R, G, B };
-				avgRGB.Add(rgbs);
-				R = 0;
-				G = 0;
-				B = 0;
-			}
-
-			foreach(int[] avgRGBS in avgRGB)
-			{
-				Console.WriteLine("{0},{1},{2}", avgRGBS[0], avgRGBS[1], avgRGBS[2]);
-			}
-			return null;
+            
+            return null;
 		}
+
+        private List<int[]> avgRGB  (List<Bitmap> list)
+        {
+            List<int[]> rgbAVG = new List<int[]>();
+            foreach (Bitmap bitmap in list)
+            {
+                int R = 0;
+                int G = 0;
+                int B = 0;
+                int cont = 0;
+                for (int i = 0; i < bitmap.Height; i++)
+                {
+
+                    for (int j = 0; j < bitmap.Width; j++)
+                    {
+                        Color now_color = bitmap.GetPixel(j, i);
+                        R += (int)now_color.R;
+                        G += (int)now_color.G;
+                        B += (int)now_color.B;
+                        cont++;
+                    }
+                }
+                int[] RGB = { R / cont, G / cont, B / cont };
+                rgbAVG.Add(RGB);
+            }
+            return rgbAVG;
+        }
+
+        private int[] avgRGB(Bitmap bitmap)
+        {
+
+                int R = 0;
+                int G = 0;
+                int B = 0;
+                int cont = 0;
+                for (int i = 0; i < bitmap.Height; i++)
+                {
+
+                    for (int j = 0; j < bitmap.Width; j++)
+                    {
+                        Color now_color = bitmap.GetPixel(j, i);
+                        R += (int)now_color.R;
+                        G += (int)now_color.G;
+                        B += (int)now_color.B;
+                        cont++;
+                    }
+                }
+                int[] RGB = { R / cont, G / cont, B / cont };
+
+            
+            return RGB;
+        }
 
     }
 }
+
